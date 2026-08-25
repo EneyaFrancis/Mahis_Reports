8386698d253c12ec879be596191da9edd3d4eb09
EneyaFrancis <franciseneya05@gmail.com>
2026-08-25 14:16:24 +0200

    alligned ui with minor issues

--- mnid/core/cache.py
+++ mnid/core/cache.py
@@ -22,8 +22,8 @@ _EXECUTIVE_CACHE_DIR = os.environ.get('MNID_EXEC_CACHE_DIR') or os.path.join(
 )
 _MNID_EXECUTIVE_DISK_CACHE = diskcache.Cache(_EXECUTIVE_CACHE_DIR, size_limit=512 * 1024 * 1024)
 _MNID_WARNED_MESSAGES: set = set()
-_COUNTRY_PROFILE_RENDER_VERSION = "country-profile-v19-png-title-filename"
-_EXECUTIVE_RENDER_VERSION = "executive-v15-png-title-filename"
+_COUNTRY_PROFILE_RENDER_VERSION = "country-profile-v23-css-page-width"
+_EXECUTIVE_RENDER_VERSION = "executive-v20-topbar-match"
 
 
 _network_df_cache: dict = {}
